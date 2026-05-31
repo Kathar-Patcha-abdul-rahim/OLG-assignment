bc608bccd3da884100b14bf8ab34519c679eb73d
Kathar-Patcha-abdul-rahim <abdulrak@uwindsor.ca>
2026-05-31 13:36:06 -0400

    Added Screenshot Util for taking screenshots

---
+++ src/test/java/utils/ScreenshotUtil.java
@@ -0,0 +1,15 @@
+package utils;
+
+import com.microsoft.playwright.Page;
+
+import java.nio.file.Paths;
+
+public class ScreenshotUtil {
+
+    public static void takeScreenshot(Page page, String testName)
+    {
+        page.screenshot(new Page.ScreenshotOptions()
+                .setPath(Paths.get("screenshots/" + testName + ".png"))
+                .setFullPage(true));
+    }
+}
